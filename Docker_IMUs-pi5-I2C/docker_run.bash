cca24bab4c95a6377ce61fc44492a345decfe0d2
Edoardo Giacardi <edoardogiaca2001@gmail.com>
2025-02-20 11:43:51 +0100

    Add files via upload
    Initial set for the docker folder. Configuration to build a Docker image suitable for deployment on a Raspberry Pi 5, ensuring a consistent environment for running the ROS 2 nodes.

---
+++ Docker_IMUs-pi5-I2C/docker_run.bash
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# ===========================================
+# docker_run.bash
+# ===========================================
+# This script runs the container interactively
+# and removes it upon exit, providing some
+# typical options like X11 forwarding for GUI.
+# =============================
+
+# Immediately exit if any command within the script returns a non-zero exit status
+set -e
+
+# Define necessary variables directly in the script
+IMAGE="ros2_mipo"    # Docker image name (should match the one built via docker_build.bash)
+TAG="latest"                             # Docker image tag
+CMD_INTERACTIVE="/bin/bash"           # Command to run in interactive mode
+HOSTNAME="localhost"                  # Hostname for the container
+USERNAME="ros"                        # User name inside the container
+WORKSPACE="ros2_ws"                   # Workspace directory inside the container
+CONTAINER_NAME="ros2_mipo_container"  # Name of the container
+
+echo "Running container: $CONTAINER_NAME"
+
+# Allow local docker containers to connect to X server (if not done previously)
+xhost +local:docker
+
+
+# Run docker    
+docker run -it --rm \
+  --hostname $HOSTNAME \
+  --name "${CONTAINER_NAME}" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --privileged \
+  --net=host \
+  -w /home/$USERNAME/$WORKSPACE \
+  -v /dev:/dev \
+  -e DISPLAY=$DISPLAY \
+  "${IMAGE}:${TAG}" \
+  "$CMD_INTERACTIVE" 
